a7cefa1c030269e7f7deb83328ee2fcc8c8c42d7
SUDHAN J <107675078+samsudhan23@users.noreply.github.com>
2024-07-15 15:08:17 +0530

    Add files via upload

---
+++ js/couter.js
@@ -0,0 +1,13 @@
+function increment() {
+    document.getElementById('final').innerHTML++
+
+}
+
+function decrement() {
+    document.getElementById('final').innerHTML--
+    console.log('12');
+}
+
+function reset() {
+    document.getElementById('final').innerHTML = 0;
+}
